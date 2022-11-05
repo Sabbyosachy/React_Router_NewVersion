36bf32347da4246eb45efcfc8cda71c4d53a0d41
Sabbyosachy <subbyosachychakraborty@gmail.com>
2022-11-05 19:32:36 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Componants/About/About";
+import Home from "./Componants/Home/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
